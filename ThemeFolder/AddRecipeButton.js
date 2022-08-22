1f0246cf5877d7a474e6fe46babfe1530c5f6f28
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-08-22 16:29:56 +0100

    test

--- ThemeFolder/AddRecipeButton.js
+++ ThemeFolder/AddRecipeButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import Link from 'next/link';
+//
 function AddRecipeButton(props) {
   return (
     <Link href="/createrecipe">
